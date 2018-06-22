ad491383587b4b02a5b6611d3948a60ed7011855
vagrant <vagrant@homestead>
2018-06-22 05:53:23 +0000

    改动

--- app/Http/Controllers/Api/Webhook.php
+++ app/Http/Controllers/Api/Webhook.php
@@ -14,11 +14,7 @@ class Webhook extends Controller
         $key = config('services.webhooks.key');
         if (hash_equals('sha1=' . hash_hmac('sha1', file_get_contents("php://input"), $key),
             $request->server('HTTP_X_HUB_SIGNATURE', ''))) {
-            $env = config('app.env');
-            $no_dev = '';
-            if ($env == 'production')
-                $no_dev = '--no-dev';
-            echo shell_exec("cd {$path} && git checkout {$branch} && php artisan migrate --force && php /usr/local/bin/composer install {$no_dev} 2>&1");
+            echo shell_exec("cd {$path} && git checkout {$branch} 2>&1");
         }
         exit("done " . date('Y-m-d H:i:s', time()));
     }
