fa8894878d7739a8882c7b266e68d13d5f6786e6
vagrant <vagrant@homestead>
2018-06-22 05:58:45 +0000

    改动

--- app/Http/Controllers/Api/Webhook.php
+++ app/Http/Controllers/Api/Webhook.php
@@ -14,7 +14,7 @@ class Webhook extends Controller
         $key = config('services.webhooks.key');
         if (hash_equals('sha1=' . hash_hmac('sha1', file_get_contents("php://input"), $key),
             $request->server('HTTP_X_HUB_SIGNATURE', ''))) {
-            echo shell_exec("cd {$path} && git checkout {$branch} 2>&1");
+            echo shell_exec("cd {$path} && git checkout {$branch} 2>&1 && git pull");
         }
         exit("done " . date('Y-m-d H:i:s', time()));
     }
